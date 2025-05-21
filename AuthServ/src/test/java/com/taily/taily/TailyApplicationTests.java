225d04560914b2bcd5c3559ae9ace954b064ee49
HotTastyRamen <yaremdan@gmail.com>
2025-05-21 19:35:22 +0300

    Initial

---
+++ AuthServ/src/test/java/com/taily/taily/TailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taily.taily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
